30ab8c6ed6000455809dfbb0bd34a289b26626bb
fswebdev <47744494+fswebdev@users.noreply.github.com>
2021-08-27 18:29:54 +0530

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:8080/api/auth/";
-const API_URL = "http://authapp1.netlify.app/api/auth/";
+const API_URL = "http://localhost:8080/api/auth/";
+// const API_URL = "http://authapp1.netlify.app/api/auth/";
 
 class AuthService {
   login(username, password) {
